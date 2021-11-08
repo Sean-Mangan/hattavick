d535e87ba9deee9a198c4cfcda4874093dd7c312
Sean Mangan <spmangan2000@gmail.com>
2021-11-08 17:54:54 -0600

    testing automated deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>my dick</code> and save to reload.
+          Edit my dick and save to reload.
         </p>
         <a
           className="App-link"
