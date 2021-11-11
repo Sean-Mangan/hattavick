01ac47c5acd4f38d92e4bdb731b7e45b4afaa4c6
Sean Mangan <spmangan2000@gmail.com>
2021-11-10 22:38:51 -0600

    stinkman

--- src/pages/MyCharacterStats/MyCharacterStats.js
+++ src/pages/MyCharacterStats/MyCharacterStats.js
@@ -16,6 +16,7 @@ const MyCharacterStats = () => {
     return (
         <div style={{textAlign: 'center'}}>
             <h1> My Character is {(character) ? character.name : ''} </h1>
+            <h3>My Characters description is {(character) ? character.description : ''}</h3>
         </div>
     )
 }
