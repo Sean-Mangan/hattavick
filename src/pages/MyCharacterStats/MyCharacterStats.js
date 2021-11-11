73cf219fc926572e65623ac9bef6b184bdec15aa
Sean Mangan <spmangan2000@gmail.com>
2021-11-10 21:52:02 -0600

    added db connection

--- src/pages/MyCharacterStats/MyCharacterStats.js
+++ src/pages/MyCharacterStats/MyCharacterStats.js
@@ -1,48 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const MyCharacterStats = () => {
+
+    var [character, setCharacter] = useState();
+
+    useEffect(() => {
+
+        axios.get('https://f12u17d0a5.execute-api.us-east-1.amazonaws.com/dev/characters')
+          .then(res => {
+            setCharacter(res.data.message);
+          })
+      }, [])
+
     return (
-        <div>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
-            <h1>MyCharacterStat</h1>
-            <br/>
+        <div style={{textAlign: 'center'}}>
+            <h1> My Character is {(character) ? character.name : ''} </h1>
         </div>
     )
 }
