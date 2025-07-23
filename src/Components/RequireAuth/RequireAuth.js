996f4a8e5d3f65ad84485ee2ea472305885b8f9d
Sean Mangan <spmangan2000@gmail.com>
2025-07-22 22:38:38 -0500

    fix:

--- src/Components/RequireAuth/RequireAuth.js
+++ src/Components/RequireAuth/RequireAuth.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import {
   useLocation,
@@ -11,12 +11,23 @@ import {
   selectCurrentPermissions,
   selectCurrentToken,
 } from "../../features/auth/authSlice";
+import { useRefreshMutation } from "../../features/auth/authApiSlice";
 
 const RequireAuth = () => {
   const token = useSelector(selectCurrentToken);
   const permissions = useSelector(selectCurrentPermissions);
   const location = useLocation();
   const context = useOutletContext();
+
+  // Heartbeat: refresh token every 60 seconds if authenticated
+  const [refresh] = useRefreshMutation();
+  useEffect(() => {
+    if (!token) return;
+    const interval = setInterval(() => {
+      refresh();
+    }, 60000); // 60 seconds
+    return () => clearInterval(interval);
+  }, [token, refresh]);
 
   return (
     <>
